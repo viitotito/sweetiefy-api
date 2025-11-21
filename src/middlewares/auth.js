30c7dd29642ecf47bfdbf22426bc694cb4f37d52
viitotito <viitomontunes@gmail.com>
2025-11-21 16:15:45 -0300

    implementado auth.js

---
+++ src/middlewares/auth.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+const { JWT_ACCESS_SECRET } = process.env;
+
+export function authMiddleware(req, res, next) {
+    try {
+
+        const authorization_header = req.headers["authorization"];
+
+        if (!authorization_header || !authorization_header.startsWith("Bearer ")) {
+            return res.status(401).json({ erro: "token ausente" });
+        }
+
+        const token = authorization_header.slice(7);
+
+        const payload = jwt.verify(token, JWT_ACCESS_SECRET);
+
+        req.user = { id: payload.sub, papel: payload.papel, nome: payload.nome };
+
+        next();
+    } catch {
+        return res.status(401).json({ erro: "token inválido" });
+    }
+}
