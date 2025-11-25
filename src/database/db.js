d78ce2dba72c13534571716f12c8a7f2255046b7
viitotito <viitomontunes@gmail.com>
2025-11-25 17:55:27 -0300

    modificado config de conexao

--- src/database/db.js
+++ src/database/db.js
@@ -1,14 +1,9 @@
-import { Pool } from "pg";
-import dotenv from "dotenv";
+import pkg from "pg";
+const { Pool } = pkg;
 
-dotenv.config();
-
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
-
-export { pool };
